c0ab072d5c29377c3051fee03ec18f754ef3091a
ryuryu1090122 <ryuryu1090122@gmail.com>
2023-02-27 22:17:42 +0900

    first commit

---
+++ Source/PluginEditor.cpp
@@ -0,0 +1,39 @@
+/*
+  ==============================================================================
+
+    This file contains the basic framework code for a JUCE plugin editor.
+
+  ==============================================================================
+*/
+
+#include "PluginProcessor.h"
+#include "PluginEditor.h"
+
+//==============================================================================
+HelloSamplerAudioProcessorEditor::HelloSamplerAudioProcessorEditor (HelloSamplerAudioProcessor& p)
+    : AudioProcessorEditor (&p), audioProcessor (p)
+{
+    mLoadButton.onClick = [&]();
+    setSize (200, 200);
+}
+
+HelloSamplerAudioProcessorEditor::~HelloSamplerAudioProcessorEditor()
+{
+}
+
+//==============================================================================
+void HelloSamplerAudioProcessorEditor::paint (juce::Graphics& g)
+{
+    // (Our component is opaque, so we must completely fill the background with a solid colour)
+    g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
+
+    g.setColour (juce::Colours::white);
+    g.setFont (15.0f);
+    g.drawFittedText ("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
+}
+
+void HelloSamplerAudioProcessorEditor::resized()
+{
+    // This is generally where you'll want to lay out the positions of any
+    // subcomponents in your editor..
+}
---
+++ Source/PluginEditor.h
@@ -0,0 +1,32 @@
+/*
+  ==============================================================================
+
+    This file contains the basic framework code for a JUCE plugin editor.
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include <JuceHeader.h>
+#include "PluginProcessor.h"
+
+//==============================================================================
+/**
+*/
+class HelloSamplerAudioProcessorEditor  : public juce::AudioProcessorEditor
+{
+public:
+    HelloSamplerAudioProcessorEditor (HelloSamplerAudioProcessor&);
+    ~HelloSamplerAudioProcessorEditor() override;
+
+    //==============================================================================
+    void paint (juce::Graphics&) override;
+    void resized() override;
+
+private:
+    juce::TextButton mLoadButton{"Load"};
+    HelloSamplerAudioProcessor& audioProcessor;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (HelloSamplerAudioProcessorEditor)
+};
